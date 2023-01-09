e3efbd40ad8d961221d5af027e9a37d00479c001
nabeel-6d <mahmood.nabeel@6dtech.co.in>
2023-01-09 11:08:48 +0530

    committed

--- src/test/java/com/example/log4j2impl/Log4j2implApplicationTests.java
+++ src/test/java/com/example/log4j2impl/Log4j2implApplicationTests.java
@@ -1,9 +1,11 @@
 package com.example.log4j2impl;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@SpringBootConfiguration
 class Log4j2implApplicationTests {
 
 	@Test
